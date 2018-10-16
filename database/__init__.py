63f027cf84bea863f7d8dddb063370f5e638607e
ch3njust1n <ch3njus@gmail.com>
2018-10-16 15:38:35 -0400

    Updated database templates

--- database/__init__.py
+++ database/__init__.py
@@ -12,3 +12,54 @@ from pymongo import MongoClient
 class Database(object):
 	def __init__(self, host='localhost', port=27017):
 		self.client = MongoClient(host, port)
+
+
+	def search(self, term):
+		self.collection.find(term)
+
+
+	def remove(self, term):
+		doc = self.search(term)
+		self.collection.delete_one(doc)
+
+
+class KnowledgeDB(Database):
+	def __init__(self, host='localhost', port=27017):
+		Database.__init__(self)
+		self.collection = self.client.knowledge
+
+
+	def search(self, data):
+		self.search(term)
+
+
+	def remove(self, term):
+		self.remove(term)
+
+
+class UserDB(Database):
+	def __init__(self, host='localhost', port=27017):
+		Database.__init__(self)
+		self.collection = self.client.user
+
+
+	def search(self, term):
+		self.search(term)
+
+
+	def remove(self, term):
+		self.remove(term)
+
+
+class CommunityDB(Database):
+	def __init__(self, host='localhost', port=27017):
+		Database.__init__(self)
+		self.collection = self.client.community
+
+
+	def search(self, term):
+		self.search(term)
+
+
+	def remove(self, term):
+		self.remove(term)
