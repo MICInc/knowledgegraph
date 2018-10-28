452b51429e4a94ee649b173ff17873a822b94d63
ch3njust1n <ch3njus@gmail.com>
2018-10-28 17:47:11 -0400

    Added account and relation schemas

--- database/schemas/account.py
+++ database/schemas/account.py
@@ -1,1 +1,15 @@
-attributes = {}
+attributes = {
+	'date_created': str,
+	'last_updated': str,
+	'first_name': str,
+	'last_name': str,
+	'username': str,
+	'email': str,
+	'bio': str,
+	'liked_articles': [],
+	'saved_articles': [],
+	'liked_papers': [],
+	'saved_papers': [],
+	'search_history': [],
+	'following': [],
+	'rank': int}
--- database/schemas/relation.py
+++ database/schemas/relation.py
@@ -1,1 +1,6 @@
-attributes = {}
+attributes = {
+	'entity_1': str,
+	'entity_2': str,
+	'direction': int, # -1, 0, 1
+	'relation': str,
+}
