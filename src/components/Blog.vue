a0b3a39ed5f87fd926fe15f756e56698e60570b0
ch3njust1n <ch3njus@gmail.com>
2018-11-18 15:13:29 -0500

    Added blog component

---
+++ src/components/Blog.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="add-blog">
+		<h2>Add a New Blog Post</h2>
+		<form>
+			<lable>Blog Title:</lable>
+			<input type="text" required/>
+			<label>Blog Content:</label>
+			<textarea></textarea>
+		</form>
+		<div id="preview">
+			<h3>Preview Blog</h3>
+			<p>Blog Title:</p>
+			<p>Blog Content:</p>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+	data() {
+		return {
+
+		}	 
+	},
+	methods: {
+
+	}
+}
+</script>
+<style>
+	body {
+		margin: 0;
+		font-family: 'Nunito SemiBold';
+	}
+</style>
