dc0b520d1bd6dfd93e53ebb053eff5f08a74f289
ddehueck <d.dehueck@gmail.com>
2018-11-18 19:02:49 -0500

    Added foundation from today

---
+++ kg-frontend/src/components/LinkedArticle.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="linked-article-wrapper">
+		<div class="left">
+			<h2>{{article.title}}</h2>
+			<p>{{article.description}}</p>
+			<div class="actions">
+				<div class="liked">{{article.num_liked}} Liked</div>
+				<div class="shared">{{article.num_shared}} shared</div>
+				<div class="comments">{{article.num_comments}} comments</div>
+			</div>
+		</div>
+		<div class="right">
+			<img src="https://picsum.photos/300/200/?random">
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'LinkedArticle',
+	props: {
+		article: Object,
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.linked-article-wrapper {
+	display: flex;
+	max-width: 600px;
+	height: 200px;
+	border: 1px solid #000;
+	margin-bottom: 25px;
+}
+
+.left, .right {
+	width: 300px;
+}
+
+.left {
+	padding: 10px;
+}
+
+.actions {
+	display: flex;
+}
+
+.actions div {
+	margin: 5px 5px;
+}
+
+.actions div:first-child {
+	margin-left: 0;
+}
+
+
+
+</style>
--- kg-frontend/src/views/Home.vue
+++ kg-frontend/src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<PageNav></PageNav>
+		<div class="linked-articles-list">
+			<div class="linked-article" v-for="article in articles" :key="article.id">
+				<LinkedArticle :article="article"></LinkedArticle>	
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PageNav from '@/components/PageNav.vue'
+import LinkedArticle from '@/components/LinkedArticle.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+	name: 'home',
+	components: {
+		LinkedArticle,
+		PageNav,
+	},
+
+	data () {
+		return {
+			articles:[
+				{
+					id: 0,
+					title: 'This is a title',
+					description: 'This is a description',
+					num_liked: 102,
+					num_shared: 22,
+					num_commented: 19,
+				},
+				{
+					id: 1,
+					title: 'This is a second title',
+					description: 'This is a second description',
+					num_liked: 230,
+					num_shared: 34,
+					num_commented: 56,
+				},
+			],
+		}
+	}
 }
 </script>
+
+
+<style scoped>
+	
+.linked-articles-list {
+	margin: 25px 0;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+</style>
