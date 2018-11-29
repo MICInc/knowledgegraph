ed3e389e5fd524588ee8638d454af153c919fc67
ch3njust1n <ch3njus@gmail.com>
2018-11-29 14:13:19 -0500

    Can now save communities to mongo

--- backend/routes/community.js
+++ backend/routes/community.js
@@ -4,6 +4,23 @@ var db = require('../db/database');
 
 router.post('/', function(req, res) {
 	console.log(req.body);
+	var community = new db.Community(req.body);
+
+	community.collection.dropIndexes(function(err, results) {
+		if(err) {
+			console.log('community.js: '+err);
+		}
+	});
+
+	community.save()
+	.then(item => {
+		console.log('Saved');
+		res.send('Community');
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(400).send('Save error');
+	});
 });
 
 module.exports = router;
