922de85e98c30522a12ef8e10f542e66d6193322
ch3njust1n <ch3njus@gmail.com>
2019-02-21 11:48:02 -0500

    Fixed date validation wow super easy thanks to this blog https://medium.com/@esganzerla/simple-date-validation-with-javascript-caea0f71883c

--- frontend/src/components/DateSelector.vue
+++ frontend/src/components/DateSelector.vue
@@ -1,15 +1,15 @@
 <template>
 	<div class="date">
 		<label>Year: </label>
-		<select name="year" v-model.number="year" v-bind:input="emit()">
+		<select name="year" v-model.number="year" :input="emit()">
 			<option v-for="year in form.years">{{ year }}</option>
 		</select>
 		<label>  Month: </label>
-		<select name="month" v-model.number="month" v-bind:input="emit()">
+		<select name="month" v-model.number="month" :input="emit()">
 			<option v-for="(value, index) in 12">{{ value }}</option>
 		</select>
 		<label>  Day: </label>
-		<select name="day" v-model.number="day" v-bind:input="emit()">
+		<select :class="{ error: error }" name="day" v-model.number="day" :input="emit()">
 			<option v-for="(value, index) in 31">{{ value }}</option>
 		</select>
 	</div>
@@ -29,13 +29,16 @@ export default {
 			},
 			day: 0,
 			month: 0,
-			year: 0
+			year: 0,
+			error: false
 		}
 	},
 	methods: {
 		emit() {
-			if(this.day > 0 && this.month > 0 && this.year > 0) {
-				this.$emit('date', new Date(this.year, this.month, this.day));
+			const date = new Date(`${this.year}-${this.month}-${this.day}`);
+			if(this.year > 0 && this.month > 0 && this.day > 0) {
+				if(date.getDate() == this.day) this.$emit('date', date);
+				else this.error = true;
 			}
 		}
 	}
@@ -45,5 +48,16 @@ export default {
 <style>
 label {
 	color: #606060;
+}
+
+select {
+	border: transparent;
+	border-width: 0.5px;
+}
+
+.error {
+	border: solid;
+	border-width: 0.5px;
+	border-color: red;
 }
 </style>
