1961b0a52bc6838787b5b99e6789213b4bc4e8c4
ch3njust1n <ch3njus@gmail.com>
2019-02-22 11:35:11 -0500

    Search only retrieves results if published

--- backend/lib/search_handler.js
+++ backend/lib/search_handler.js
@@ -1,11 +1,15 @@
 module.exports = {
-	filter: function(query) {
+	filter_query: function(query) {
 		return query;
 	},
+	filter_results: function(results) {
+		console.log(results);
+		return results;
+	},
 	format: function(query) {
-		var query = module.exports.filter(query);
+		var query = module.exports.filter_query(query);
 		query = { $regex: new RegExp("^" + query.toLowerCase(), "i") };
 
-		return { 'title': query };
+		return { 'title': query, 'published': true };
 	}
 }
--- backend/routes/search.js
+++ backend/routes/search.js
@@ -6,11 +6,9 @@ var sh = require('../lib/search_handler');
 router.get('/', function(req, res, next){
 	// Need user id to save search history
 	// var date = new Date();
-	console.log(req.query.term);
 
 	db.Content.find(sh.format(req.query.term.toUpperCase()), function (err, results) {
-		console.log(results);
-		res.send(results);
+		res.send(sh.filter_results(results));
 	});
 });
 
