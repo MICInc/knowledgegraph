2f156e8544b155c87d5c4da40509b282a6281f13
ch3njust1n <ch3njus@gmail.com>
2019-03-02 15:54:56 -0500

    Started page timer

---
+++ frontend/src/components/Timer.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="container"></div>
+</template>
+
+<script>
+export default {
+	beforerDestroyed() {
+		this.$emit('time', {start: this.start, stop: new Date()});
+	},
+	data() {
+		return {
+			start: new Date()
+		}
+	}
+	props: ['url'];
+}
+</script>
