8eab272f17836c60a3fb8991c6b3dcd0d9f3bb1c
ch3njust1n <ch3njus@gmail.com>
2019-03-19 10:13:55 -0400

    Manually added back backup of token.js. Have not yet integrated it with rest of code. Scared that CORS issue will happen again.

---
+++ backend/lib/token.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var jwt = require('jsonwebtoken');
+
+/*
+	Steps for generating ssh key in pem format without passphrase:
+	1. openssl genrsa -out private.pem 2048
+	2. openssl rsa -in private.pem -outform PEM -pubout -out public.pem
+*/
+const private_key = fs.readFileSync(__dirname+'/../config/private.pem', 'utf8');
+const public_key = fs.readFileSync(__dirname+'/../config/public.pem', 'utf8');
+
+module.exports = {
+	decode: function(token) {
+		return jwt.decode(token, {complete: true});
+	},
+	sign: function(payload, user) {
+		var signOpt = {
+			issuer: "Machine Intelligence Community",
+			subject: user,
+			audience: "http://machineintelligence.cc",
+			expiresIn: "24h",
+			algorithm: "RS256"
+		}
+
+		return jwt.sign(payload, private_key, signOpt);
+	},
+	verify: function(token, user) {
+		verifyOpt = {
+			issuer: "Machine Intelligence Community",
+			subject: user,
+			audience: "http://machineintelligence.cc",
+			expiresIn: "24h",
+			algorithms: ["RS256"]
+		}
+
+		return jwt.verify(token, public_key, verifyOpt);
+	}
+}
