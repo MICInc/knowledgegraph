4509793c0e34a1dbd9e56df91cf321958f17d5ce
ch3njust1n <ch3njus@gmail.com>
2019-04-12 10:46:56 -0400

    Updated terms and updated data

--- frontend/src/views/about/Data.vue
+++ frontend/src/views/about/Data.vue
@@ -1,7 +1,14 @@
 <template>
 	<div class="container">
 		<PageNav></PageNav>
-		<h2>Data</h2>
+		<h1>Data Policy</h1>
+		<ol class="term-info">
+			<li v-for="(value, index) in terms">
+				<span class="terms-sect-header">{{ value.title }}</span>
+				<p>{{ value.desc }}</p>
+			</li>
+		</ol>
+		<span class="dated">DATED: March 31st, 2019</span>
 	</div>
 </template>
 
@@ -15,7 +22,11 @@ export default {
 	},
 	data() {
 		return {
+			policy: [
+				{
 
+				}
+			]
 		}
 	}
 }
