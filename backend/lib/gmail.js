650546277901b58d0cbb0b94f3afb90910d57e36
ch3njust1n <ch3njus@gmail.com>
2019-04-25 15:11:55 -0400

    Saving progress. Updated gmail, reparameterized sendMessage() and removed log statements

--- backend/lib/gmail.js
+++ backend/lib/gmail.js
@@ -25,13 +25,13 @@ function makeBody(to, from, subject, message) {
         message
     ].join('');
 
-    var encodedMail = new Buffer(str).toString("base64").replace(/\+/g, '-').replace(/\//g, '_');
+    var encodedMail = new Buffer.from(str, 'utf8').toString("base64").replace(/\+/g, '-').replace(/\//g, '_');
         return encodedMail;
 }
 
-function sendMessage(auth) {
+function sendMessage(auth, to, subject, from='admin@machineintelligence.cc') {
     const gmail = google.gmail({version: 'v1', auth});
-    var raw = makeBody('admin@machineintelligence.cc', 'ch3njus@gmail.com', 'Test', 'message test');
+    var raw = makeBody(from, to, subject, message);
     gmail.users.messages.send({
         auth: auth,
         userId: 'me',
@@ -39,7 +39,7 @@ function sendMessage(auth) {
             raw: raw
         }
     }, function(err, response) {
-        res.send(err || response)
+      if(err) console.error(err);
     });
 }
 
@@ -99,26 +99,3 @@ function getNewToken(oAuth2Client, callback) {
     });
   });
 }
-
-/**
- * Lists the labels in the user's account.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-// function listLabels(auth) {
-//   const gmail = google.gmail({version: 'v1', auth});
-//   gmail.users.labels.list({
-//     userId: 'me',
-//   }, (err, res) => {
-//     if (err) return console.log('The API returned an error: ' + err);
-//     const labels = res.data.labels;
-//     if (labels.length) {
-//       console.log('Labels:');
-//       labels.forEach((label) => {
-//         console.log(`- ${label.name}`);
-//       });
-//     } else {
-//       console.log('No labels found.');
-//     }
-//   });
-// }
