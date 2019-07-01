3b27a0b92c71ca81300d60836d0d01d2b1ea391b
ch3njust1n <ch3njus@gmail.com>
2019-07-01 00:30:12 -0400

    Removed error because socials are optional

--- frontend/src/components/conference/registration/Skillsheet.vue
+++ frontend/src/components/conference/registration/Skillsheet.vue
@@ -1,8 +1,8 @@
 <template>
 	<div v-on:keyup="update()">
 		<label>Opt-in to <a href="https://www.dropbox.com/request/UEtnUpmQ5AHMOw8PmrvN" target="_blank">share</a> your skill sheet with sponsors</label><br>
-		<input :class="{ error: error }" type="text" placeholder="LinkedIn (optional)" v-model.trim="social.linkedin"><br>
-		<input :class="{ error: error }" type="text" placeholder="Github (optional)" v-model.trim="social.github">
+		<input type="text" placeholder="LinkedIn (optional)" v-model.trim="social.linkedin"><br>
+		<input type="text" placeholder="Github (optional)" v-model.trim="social.github">
 	</div>
 </template>
 
